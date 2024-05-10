4229400c78e6289cb9a6c60fdb309cacdb67b2a1
JiaqiCUI666 <jiaqicui666@qq.com>
2024-05-10 22:19:15 +0800

    加入了快速排序代码

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,36 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    public static void quick_sort(int[] array, int low, int high) {
+        int begin = low;
+        int end = high;
+        int key = begin;
+        int temp;
+
+        if (low >= high) {
+            return;
+        }
+
+        while (begin < end) {
+            while (begin < end && array[end] >= array[key]) {
+                end -= 1;
+            }
+
+            while (begin < end && array[begin] <= array[key]) {
+                begin += 1;
+            }
+
+            temp = array[begin];
+            array[begin] = array[end];
+            array[end] = temp;
+        }
+
+        temp = array[begin];
+        array[begin] = array[key];
+        array[key] = temp;
+        quick_sort(array, low, begin - 1);
+        quick_sort(array, begin + 1, high);
+        return;
+    }
 }
